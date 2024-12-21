e840f1155381cd6f7d0c50f17d0eb6a6e26902b3
Arseniy <arseniygoryagin@gmail.com>
2024-12-21 22:01:56 +0300

    added no args constructor

--- src/main/java/com/biblebot/callbackdata/CallBackData.java
+++ src/main/java/com/biblebot/callbackdata/CallBackData.java
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-
+import lombok.NoArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CallBackData {
 
     private Pages page;
